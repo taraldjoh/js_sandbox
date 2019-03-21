481de8f65214dd27c60c90b724db208899f6edfe
taraldjoh <tarald.johansen@outlook.com>
2019-03-22 00:33:47 +0100

    fixed code for posting the text document

--- ajax_xml/app.js
+++ ajax_xml/app.js
@@ -8,12 +8,20 @@ function loadData() {
   xhr.open("GET", "text.txt", true);
 
   xhr.onload = function() {
+    console.log("READYSTATE", xhr.readyState);
     // Check if status is = 200 (200 status = all is ok)
     if (this.status === 200) {
+      // document.getElementById("output").innerHTML = `<h1>${
+      //   this.responseText
+      // }</h1>`;
       document.getElementById("output").innerHTML = `<h1>${
         this.responseText
       }</h1>`;
     }
+  };
+
+  xhronerror = function() {
+    console.log("Request Error...");
   };
 
   xhr.send();
