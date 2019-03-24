a86c79d1c4637c6965429c48bed4ba0d18f67adf
taraldjoh <tarald.johansen@outlook.com>
2019-03-24 10:54:07 +0100

    fetches .json data and outputs in customer ul

---
+++ ajax_json/app.js
@@ -0,0 +1,29 @@
+document.getElementById("button1").addEventListener("click", loadCustomer);
+
+function loadCustomer(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "customer.json", true);
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      //console.log(this.responseText);
+
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>NAME: ${customer.name}</li>
+          <li>COMPANY: ${customer.company}</li>
+          <li>PHONE: ${customer.phone}</li>
+        </ul>
+      `;
+
+      // OUtput to browser
+      document.getElementById("customer").innerHTML = output;
+    }
+  };
+
+  xhr.send();
+}
