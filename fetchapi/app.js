724543dddf6f98cc03b6ffa3e6e910bf026f387f
Tarald Johansen <41441415+taraldjoh@users.noreply.github.com>
2019-04-07 11:59:37 +0200

    refactored with arrow functions

--- fetchapi/app.js
+++ fetchapi/app.js
@@ -6,52 +6,41 @@ document.getElementById("button3").addEventListener("click", getApi);
 function getText() {
     fetch('data.txt')
     
-    .then(function(res){
-        return res.text();
-    })
-    .then(function(data) {
+    .then(res => res.text())
+    
+    .then(data => {
         console.log(data);
         document.getElementById("output").innerHTML = data;
     })
-    .catch(function(err){
-        console.log(err);
-    })
+    .catch(err => console.log(err))
 }
 
 // Get local JSON data
 function getJson() {
     fetch('post.json')
-        .then(function(res){
-            return res.json();
-        })
-        .then(function(data) {
+        .then(res => res.json())
+        .then(data => {
             console.log(data);
             let output = "";
-            data.forEach(function(post) {
+            data.forEach(post => {
                 output += `<li>${post.title}</li>`
             });
             document.getElementById("output").innerHTML = output;
         })
-        .catch(function(err){
-            console.log(err);
-        })
+        .catch(err => console.log(err))
 }
 
 // Get API Data
 function getApi() {
     fetch('https://api.github.com/users')
-        .then(function(res){
-            return res.json();
-        })
-        .then(function(data) {
+        .then(res => res.json())
+        .then(data => {
             console.log(data);
             let output = "";
-            data.forEach(function(user) {
+            data.forEach(user => {
                 output += `<li>${user.login}</li>`
             });
             document.getElementById("output").innerHTML = output;
         })
-        .catch(function(err){
-            console.log(err);
-        })
+        .catch(err => console.log(err))
 }
